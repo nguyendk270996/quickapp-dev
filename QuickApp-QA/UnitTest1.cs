93a70a656e61a9a8f6c06cc841408a5349a06369
Nguyen, Khoi Dang <nguyen.khoidang@coxautoinc.com>
2021-10-08 07:46:30 +0700

    Init

---
+++ QuickApp-QA/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Allure.Commons;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+
+namespace QuickApp_QA
+{
+    [TestFixture]
+    [AllureNUnit]
+    [AllureSuite("BoMayThichFail")]
+    [AllureDisplayIgnored]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+        [Test(Description = "This Shall Pas")]
+        [AllureTag("CI")]
+        [AllureSeverity(SeverityLevel.critical)]   
+        public void ThisShallPass()
+        {
+            Assert.Pass("Ez gameeee!!!!");
+        }       
+    }
+}
